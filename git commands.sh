084689f9f398a92f3594a8a15bd1ba6fb11a5672
Sohom Ghosh <sohom1ghosh@gmail.com>
2017-08-04 18:22:21 +0530

    Create git commands.sh

---
+++ git commands.sh
@@ -0,0 +1,13 @@
+#Create relevant project in github
+git clone <http://link_to_github_project.git>
+cd <name_of_git_folder>
+git status
+
+#Adding all files of present folder to github
+git add .
+git status
+git commit -m "comment add here"
+
+git status
+git push
+#Enter user_id and passowrd
