a27c390872b7eb0c2e9d06ae59d3ab5c94f19641
Sohom Ghosh <sohom1ghosh@gmail.com>
2017-08-18 21:05:18 +0530

    Update git commands.sh

--- git commands.sh
+++ git commands.sh
@@ -11,3 +11,13 @@ git commit -m "comment add here"
 git status
 git push
 #Enter user_id and passowrd
+
+#Seeing git commit history
+git log
+#See entries like: 
+#commit f176757hj68799hhkjga007270
+#Author: Sohom Ghosh <sohom1ghosh@gmail.com>
+#Date:   Fri Aug 4 18:31:05 2017 +0530
+
+#Reverting back to an old state
+git checkout -b old-state f176757hj68799hhkjga007270
