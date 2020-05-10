fee72bf53808377cfad53539ccd37a553a25e20a
Sohom Ghosh <sohom1ghosh@gmail.com>
2020-05-10 19:17:35 +0530

    Update git commands.sh

--- git commands.sh
+++ git commands.sh
@@ -1,6 +1,10 @@
 #Git global setup
 git config --global user.name "Sohom Ghosh"
 git config --global user.email "sohom1ghosh@gmail.com"
+git commit --amend --reset-author
+#Esc : wq
+
+cat ~/.gitconfig
 
 #Cloning New repository
 git clone http://<link>.git
@@ -34,6 +38,14 @@ git commit
 git push -u origin master
 #To Branch master set up to track remote branch master from origin.
 
+#Create a new branch
+git checkout -b feature_branch_name
+
+#Shift to a branch
+git checkout feature_branch_name
+
+#See current brach being used
+git branch -a
 
 git status
 git push
@@ -58,3 +70,43 @@ git checkout master
 git commit
 #If conflict open file and resolve it
 
+### Most recently used commands ###
+git fetch
+git pull
+git add hello.py
+git add .
+git status
+git commit -m 'message describing this commit'
+git push
+###
+
+git stash
+git push <remote> <branch_name>
+
+#how to force git pull to overwrite local files
+git fetch --all
+git reset --hard origin/<branch_name>
+
+#when upstream is not set
+git push --set-upstream origin <feature_branch_name>
+
+#To create a new local git repository
+git init
+
+#To view log of the commits which have been done
+git log
+
+#Remove already committed file
+git rm <file_name_to_be_removed>
+
+#Move a file from one location to the other
+git mv <source_path/file_name> <destination_path>
+
+#To check the differences between 2 commits
+git diff
+git diff --base <filename>
+git diff <sourcebranch> <targetbranch>
+
+#Exclude some files from being committed
+vi .gitignore
+#In this .gitignore file add folder / files like *.log, *.pyc
